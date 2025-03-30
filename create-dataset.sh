dbd7a64ea0d9351c1a7cb028084a0ceea7b82998
mavengences <dorienxia74@gmail.com>
2025-03-30 14:59:27 -0700

    Update create-dataset.sh

--- create-dataset.sh
+++ create-dataset.sh
@@ -10,10 +10,10 @@ CUR_DIR=$(pwd)
 set -eu
 
 cd $OUTDIR
-../download-srtm-data.sh
-../create-tiles.sh SRTM_NE_250m.tif 10 10
-../create-tiles.sh SRTM_SE_250m.tif 10 10
-../create-tiles.sh SRTM_W_250m.tif 10 20
-rm -rf SRTM_NE_250m.tif SRTM_SE_250m.tif SRTM_W_250m.tif *.rar
+#../download-srtm-data.sh
+../create-tiles.sh /code/data/SRTM_NE_250m_TIF/SRTM_NE_250m.tif 10 10
+../create-tiles.sh /code/data/SRTM_SE_250m_TIF/SRTM_SE_250m.tif 10 10
+../create-tiles.sh /code/data/SRTM_W_250m_TIF/SRTM_W_250m.tif 10 20
+rm -rf /code/data/SRTM_NE_250m_TIF/SRTM_NE_250m.tif /code/data/SRTM_SE_250m_TIF/SRTM_SE_250m.tif /code/data/SRTM_W_250m_TIF/SRTM_W_250m.tif *.rar
 
 cd $CUR_DIR
